e1b0d3c9ebcca2dc519f35853d2ae4af796b1cd4
xjdjx <xjamesdejesusx@gmail.com>
2016-01-29 11:17:11 -0800

    add back the for loop

--- bin/common.sh
+++ bin/common.sh
@@ -34,6 +34,7 @@ export_env_dir() {
   whitelist_regex=${2:-''}
   blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
       echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
       export "$e=$(cat $env_dir/$e)"
       :
